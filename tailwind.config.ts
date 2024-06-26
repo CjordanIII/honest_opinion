1afbb533028aa3cf813469e7b81da4c3bc8a6660
Clarence Jordan <clarencejordan087@gmail.com>
2024-06-25 19:11:44 -0500

    add config and color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,16 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: '#848484',
+      secondary: "#444444",
+      background: "#f4f4f4",
+      accentcolor: "#1c1c24",
+      textcolor:"#f4f4f4"
+      
+    }
   },
   plugins: [],
 };
