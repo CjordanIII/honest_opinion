850846ebaf993a7153da74d9b5e44bb576f7d836
Clarence Jordan <clarencejordan087@gmail.com>
2024-06-25 23:41:02 -0500

    add form data

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,35 @@
+// interface formData {}
 export default function signup() {
+  interface FormData {
+    get(arg0: string): String;
+    email: String;
+    username: String;
+    password: String;
+    confirmpassword: String;
+  }
+
+  interface FormObj {
+    email: String;
+    username: String;
+    password: String;
+  }
+
+  async function handleform(formData: FormData) {
+    "use server";
+    const email: String = formData.get("email");
+    const username: String = formData.get("username");
+    const password: String = formData.get("password");
+    const formObj: FormObj = {
+      email: email,
+      username: username,
+      password: password,
+    };
+    console.log(formObj);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="flex min-h-full flex-1 flex-col justify-center lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-8xl font-bold leading-9 tracking-tight text-gray-900">
             🗣️
@@ -9,7 +37,7 @@ export default function signup() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" action={handleform}>
             <div>
               <label
                 htmlFor="email"
@@ -28,6 +56,24 @@ export default function signup() {
                 />
               </div>
             </div>
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-xl font-medium leading-6 text-gray-900"
+              >
+                User Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
             <div>
               <div className="flex items-center justify-between">
@@ -37,20 +83,32 @@ export default function signup() {
                 >
                   Password
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
                   id="password"
                   name="password"
                   type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1  ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="confirmpassword"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirmpassword"
+                  name="confirmpassword"
+                  type="confirmpassword"
                   autoComplete="current-password"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1  ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -69,7 +127,7 @@ export default function signup() {
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            New to Speak Your Mind?{" "}
+            already have an account?{" "}
             <a
               href="#"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
